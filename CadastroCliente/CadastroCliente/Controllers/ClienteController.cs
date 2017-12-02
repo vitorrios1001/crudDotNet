6de4e9777cc0b888140dad881e6061fc3be09e0a
Vitor Rios <vitorluizrios@hotmail.com>
2017-12-02 20:18:42 -0200

    First

---
+++ CadastroCliente/CadastroCliente/Controllers/ClienteController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using CadastroCliente.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroCliente.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListaClientes()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult InserirNovo(ClienteModel cliente)
+        {
+
+
+
+            return View("ListaClientes");
+        }
+
+        public List<ClienteModel> RetornaListaClientes()//Função que retorna a lista de clientes
+        {
+
+            var listaClientes = new List<ClienteModel>();
+
+
+
+
+
+            return listaClientes;
+        }
+        
+        
+        
+    }
+}
+
---
+++ CadastroCliente/CadastroCliente/Models/ClienteModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroCliente.Models
+{
+    public class ClienteModel
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [StringLength(40)]
+        public string Nome { get; set; }
+        
+        [StringLength(10)]
+        public string CEP { get; set; }
+        
+        [StringLength(40)]
+        public string Endereco { get; set; }
+        
+        [StringLength(30)]
+        public string Bairro { get; set; }
+        
+        [StringLength(40)]
+        public string Cidade { get; set; }
+        
+        [StringLength(2)]
+        public string Uf { get; set; }
+        
+        public bool Status { get; set; }
+        
+
+    }
+}
